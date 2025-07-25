94510107f8e452a5186ab2fdef27a8b4d3a35cf5
Hammad-Talat <hammad.talat@maidencentury.com>
2025-07-25 12:24:10 +0500

    Fetching authors by their names, fetching books by its title and published year

--- library/views.py
+++ library/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import Student, Book, Loan ,Author
 from .serializers import StudentSerializer, BookSerializer, LoanSerializer ,AuthorSerializer 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
@@ -16,3 +19,28 @@ class LoanViewSet(viewsets.ModelViewSet):
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.prefetch_related('books').all()
     serializer_class = AuthorSerializer
+@api_view(['GET'])
+def get_author_by_name(request, name):
+    author = get_object_or_404(Author, name__iexact=name)
+    serializer = AuthorSerializer(author)
+    return Response(serializer.data)
+@api_view(['GET'])
+def filter_books_by_title(request):
+    title = request.GET.get('title', '')
+    print("Hereeeeeeeeeeeeee")
+    books = Book.objects.all()
+    if title:
+        books = books.filter(title__icontains=title)
+
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def filter_books_by_year(request):
+    year = request.GET.get('year', '')
+    books = Book.objects.all()
+    if year:
+        books = books.filter(published_year=year)
+
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
