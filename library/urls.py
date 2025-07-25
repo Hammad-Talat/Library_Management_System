22641fb1b15e495502d3a177723613031c3b52f1
Hammad-Talat <hammad.talat@maidencentury.com>
2025-07-25 12:23:02 +0500

    Urls updated

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet, BookViewSet, LoanViewSet ,AuthorViewSet 
+from .views import get_author_by_name, filter_books_by_year,filter_books_by_title
 
 router = DefaultRouter()
 router.register(r"students", StudentViewSet)
@@ -10,4 +11,7 @@ router.register(r"loans", LoanViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("author/<str:name>/", get_author_by_name),
+   path('book/by-title/', filter_books_by_title),
+    path('book/by-year/', filter_books_by_year)
 ]
